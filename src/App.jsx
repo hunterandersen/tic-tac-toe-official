532ea0eb1d289e5adc42641ca8548a5d540cc8f7
Hunter Andersen <andersen.hunter42@gmail.com>
2023-05-03 12:59:53 -0700

    Added functioning click event to add X's

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,41 @@ import "./components/style.css";
 import { useState } from "react";
 
 export default function Board() {
+  //[null,null,null,null,null,null,null,null,null]
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(index){
+    //We don't want to directly change our state variables
+    const nextSquares = squares.slice();
+    nextSquares[index] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
 
-function Square() {
-  //useState returns an array of two items
-  const [value, setValue] = useState(null);
-
-  function handleClick(){
-    console.log("Clicked");
-  }
-
-  return <button
-   className="square"
-   onClick={handleClick}
+function Square({ value, onSquareClick }) {
+  return <button 
+    onClick={onSquareClick} 
+    className="square"
   >
     {value}
   </button>;
